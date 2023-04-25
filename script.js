eab18dafcb8239300b0b74e8c97d0477cf1a3ddd
Ongcangco <POngcangco57@gmail.com>
2023-04-25 18:15:31 -0400

    add getElementById

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const numbers = [
 
 
 /*----- app's state (variables) -----*/
-let board; //array of 3 windows//
+let slots; //array of 3 windows//
 let turn; //1 or -1//
 let winner; //null = loser; 1 or -1 winner; 
 // var random = Math.floor(Math.random()* 5) + 1;
@@ -27,16 +27,30 @@ winningCombos = [
     [4, 4, 4],
     [5, 5, 5],
   ];
-
+const playBtn = document.getElementById('play'); 
+const resetBtn = document.getElementById('reset')
 /*----- event listeners -----*/
+playBtn.addEventListener("click", function() {
+});
 
+resetBtn.addEventListener('click', init);
 
 /*----- functions -----*/
+init();
+function init() {
+    slots = null;
+    turn = 1;
+    winner = null;
+    
+}
+
+
+
 function getRandomNumber() {
     return Math.floor(math.random() * 5) + 1;
 }
 
-function getElement(id) {
+function getElementById(id) {
     return document.getElementById(id);
 }
 //connect the numbers to each window?
@@ -61,20 +75,15 @@ function play() {
 }
 }
 
-function render() {
-    renderBoard();
-    renderMessage();
-    //Hide/show UI elements (controls)
-    renderControls();
-  }
+ function render() {}
+// renderBoard()
+// function renderMessage()
+//     //Hide/show UI elements (controls)
+// function renderControls();
+  
 
 
 //   function changeText() {
 //     var message = document.getElementById('message');
 //     message.innerHTML = "click";
-// }
-
-
-
-
-
+// 
